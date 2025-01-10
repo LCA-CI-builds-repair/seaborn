279cf820f566e7aff5e7b68a9fd520cd2af2470a
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-10 15:37:24 +0000

    waleko__gemini-pro-1.5_8afe__id_159

--- seaborn/_core/plot.py
+++ seaborn/_core/plot.py
@@ -43,6 +43,7 @@ from seaborn._core.rules import categorical_order
 from seaborn._compat import set_layout_engine
 from seaborn.rcmod import axes_style, plotting_context
 from seaborn.palettes import color_palette
+from seaborn.utils import check_pandoc_version
 
 from typing import TYPE_CHECKING, TypedDict
 if TYPE_CHECKING:
