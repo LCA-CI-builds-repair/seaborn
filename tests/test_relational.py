4c180af4d363bb3870eaa349d35592904dbf8a6b
ubuntu <ubuntu@ip-10-0-1-83>
2025-01-18 14:22:30 +0000

    waleko__llama-3.1-70b_7234__id_160

--- tests/test_relational.py
+++ tests/test_relational.py
@@ -584,7 +584,10 @@ class TestRelationalPlotter(Helpers):
         ydata = g.ax.lines[0].get_ydata()
         for i, label in enumerate(g.ax.get_xticklabels()):
             pos_df = long_df[long_df["a"] == label.get_text()]
+            if len(pos_df) > 0:
             expected = np.average(pos_df["y"], weights=pos_df["x"])
+        else:
+            expected = np.nan
             assert ydata[i] == pytest.approx(expected)
 
     def test_relplot_stringy_numerics(self, long_df):
